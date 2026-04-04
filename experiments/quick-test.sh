79a5a27552f4e79fbf1c1f9fed7cbc97423b3fc5
Kevin Conor <kconor@gmail.com>
2026-04-03 17:19:28 -0700

    removed code from benchmarks

--- experiments/quick-test.sh
+++ experiments/quick-test.sh
@@ -1,9 +1,9 @@
   #!/bin/bash
 
-  sudo purge
-  echo "\n\n=== CODER NEXT ==="
-  cmd="../metal_infer/infer --model ../models/Qwen3-Coder-Next-4bit --prompt 'Explain how hash tables work' --tokens 100 --k 10 --timing 2>&1"
-  eval $cmd
+  #sudo purge
+  #echo "\n\n=== CODER NEXT ==="
+  #cmd="../metal_infer/infer --model ../models/Qwen3-Coder-Next-4bit --prompt 'Explain how hash tables work' --tokens 100 --k 10 --timing 2>&1"
+  #eval $cmd
 
   sudo purge
   echo "\n\n=== 35 A3B - uniform quant ==="
